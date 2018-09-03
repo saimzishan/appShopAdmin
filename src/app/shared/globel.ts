d8b241aeea106d6ac0a34bd7258ea9173935aa74
saimzishan@gmail.com <saimzishan@gmail.com>
2018-09-03 23:15:40 +0500

    category tree

--- src/app/shared/globel.ts
+++ src/app/shared/globel.ts
@@ -1,7 +1,7 @@
 export class GLOBAL {
   public static USER_API = "http://127.0.0.1:8080/api/auth/";
   public static USER_IMAGE_API = "http://124.109.39.22:18089/onlineappshopapi";
-  // public static USER_API = "http://3fa1aad4.ngrok.io/api/auth/";
+  // public static USER_API = "http://d2803cae.ngrok.io/api/auth/";
   // public static USER_API =
   // "http://124.109.39.22:18089/onlineappshopapi/public/api/auth/";
 }
