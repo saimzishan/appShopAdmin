5c1e19b9a12969b8446e6d4b31186f570c4b92f3
saimzishan@gmail.com <saimzishan@gmail.com>
2018-09-05 14:29:21 +0500

    error halding

--- src/app/shared/globel.ts
+++ src/app/shared/globel.ts
@@ -1,6 +1,7 @@
 export class GLOBAL {
   // public static USER_API = "http://127.0.0.1:8080/api/auth/";
-  public static USER_IMAGE_API = 'http://124.109.39.22:18089/onlineappshopapi';
+  public static USER_IMAGE_API = "http://124.109.39.22:18089/onlineappshopapi";
   // public static USER_API = "http://d2803cae.ngrok.io/api/auth/";
-  public static USER_API = 'http://124.109.39.22:18089/onlineappshopapi/public/api/auth/';
+  public static USER_API =
+    "http://124.109.39.22:18089/onlineappshopapi/public/api/auth/";
 }
