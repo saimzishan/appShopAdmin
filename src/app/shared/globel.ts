c40ee9cd0776e8294d9508a3f241019113a26635
saimzishan@gmail.com <saimzishan@gmail.com>
2018-09-05 14:59:26 +0500

    merging

--- src/app/shared/globel.ts
+++ src/app/shared/globel.ts
@@ -3,10 +3,5 @@ export class GLOBAL {
   public static USER_IMAGE_API = "http://124.109.39.22:18089/onlineappshopapi";
   // public static USER_API = "http://d2803cae.ngrok.io/api/auth/";
   public static USER_API =
-<<<<<<< HEAD
-  "http://124.109.39.22:18089/onlineappshopapi/public/api/auth/";
-    // "http://124.109.39.22:18089/onlineappshopapi/public/api/auth/";
-=======
     "http://124.109.39.22:18089/onlineappshopapi/public/api/auth/";
->>>>>>> 5c1e19b9a12969b8446e6d4b31186f570c4b92f3
 }
