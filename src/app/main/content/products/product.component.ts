7aacb43fa58a5b8a78f2d2a25f11f9ebbc1b916b
osamaaay <fahad40793@gmail.com>
2018-09-08 15:16:34 +0500

    product sku's options

--- src/app/main/content/products/product.component.ts
+++ src/app/main/content/products/product.component.ts
@@ -1,45 +1,45 @@
-import { Contact } from "./../apps/contacts/contact.model";
+import { Contact } from './../apps/contacts/contact.model';
 import {
   Supplier,
   OptionSet,
   OptionValue,
   ProductVariant,
   Options
-} from "./../models/product.model";
-import { Subscription } from "rxjs/Subscription";
-import { SnotifyService } from "ng-snotify";
-import { SpinnerService } from "./../../../spinner/spinner.service";
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from "@angular/core";
-import { ProductService } from "./product.service";
-import { fuseAnimations } from "../../../core/animations";
-import "rxjs/add/operator/startWith";
-import "rxjs/add/observable/merge";
-import "rxjs/add/operator/map";
-import "rxjs/add/operator/debounceTime";
-import "rxjs/add/operator/distinctUntilChanged";
-import "rxjs/add/observable/fromEvent";
-import { Product } from "../models/product.model";
-import { FormBuilder, FormGroup, FormControl } from "@angular/forms";
-import { FuseUtils } from "../../../core/fuseUtils";
-import { MatSnackBar, MatDialog, MatDialogRef } from "@angular/material";
-import { Location } from "@angular/common";
+} from './../models/product.model';
+import { Subscription } from 'rxjs/Subscription';
+import { SnotifyService } from 'ng-snotify';
+import { SpinnerService } from './../../../spinner/spinner.service';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { ProductService } from './product.service';
+import { fuseAnimations } from '../../../core/animations';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/observable/fromEvent';
+import { Product } from '../models/product.model';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FuseUtils } from '../../../core/fuseUtils';
+import { MatSnackBar, MatDialog, MatDialogRef } from '@angular/material';
+import { Location } from '@angular/common';
 import {
   FileSystemDirectoryEntry,
   FileSystemFileEntry,
   UploadEvent,
   UploadFile
-} from "ngx-file-drop";
+} from 'ngx-file-drop';
 // import { FuseConfirmDialogComponent } from '../../../core/components/confirm-dialog/confirm-dialog.component';
 
-import { FuseConfirmDialogComponent } from "../../../core/components/confirm-dialog/confirm-dialog.component";
-import { Category } from "../models/category.model";
-import { TreeModule } from "ng2-tree";
-import { MatTableDataSource } from "@angular/material";
-import { NgSelectMultipleOption } from "@angular/forms/src/directives";
-import { FuseOptionFormDialogComponent } from "./sku-form/option-form.component";
+import { FuseConfirmDialogComponent } from '../../../core/components/confirm-dialog/confirm-dialog.component';
+import { Category } from '../models/category.model';
+import { TreeModule } from 'ng2-tree';
+import { MatTableDataSource } from '@angular/material';
+import { NgSelectMultipleOption } from '@angular/forms/src/directives';
+import { FuseOptionFormDialogComponent } from './sku-form/option-form.component';
 // import * as $ from 'jquery';
-import { TreeModel, Ng2TreeSettings } from "ng2-tree";
-import { Router } from "@angular/router";
+import { TreeModel, Ng2TreeSettings } from 'ng2-tree';
+import { Router } from '@angular/router';
 const treeSettings: Ng2TreeSettings = {
   rootIsVisible: false
 };
@@ -48,9 +48,9 @@ declare var $: any;
 
 @Component({
   // tslint:disable-next-line:component-selector
-  selector: "app-product",
-  templateUrl: "./product.component.html",
-  styleUrls: ["./product.component.scss"],
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
   encapsulation: ViewEncapsulation.None,
   animations: fuseAnimations
 })
@@ -59,6 +59,7 @@ export class ProductComponent implements OnInit, OnDestroy {
   supplier: Supplier = new Supplier();
   product_variant: ProductVariant = new ProductVariant();
   options: Options = new Options();
+  sku_options: Options[];
   tOptions: Options[] = [];
   option_id = -1;
   option_set_id = [];
@@ -117,9 +118,9 @@ export class ProductComponent implements OnInit, OnDestroy {
         if (product) {
           this.product = new Product(product);
           console.log(this.product);
-          this.pageType = "edit";
+          this.pageType = 'edit';
         } else {
-          this.pageType = "new";
+          this.pageType = 'new';
           this.product = new Product();
         }
       }
@@ -140,7 +141,7 @@ export class ProductComponent implements OnInit, OnDestroy {
         const temp = this.changeFieldName(category);
 
         this.tree = {
-          value: "Categories",
+          value: 'Categories',
           children: temp
         };
         // console.log(temp);
@@ -156,7 +157,7 @@ export class ProductComponent implements OnInit, OnDestroy {
   }
 
   toogleChild(id) {
-    let temp: any = document.getElementById("ulLi" + id);
+    const temp: any = document.getElementById('ulLi' + id);
     temp.hidden = !temp.hidden;
     // temp = temp[id].children;
     // for (let index = 1; index < temp.length; index++) {
@@ -170,6 +171,17 @@ export class ProductComponent implements OnInit, OnDestroy {
     this.supplier = new Supplier();
     this.supplierForm.reset();
     console.log(this.product.suppliers);
+  }
+
+  addSKU(form) {
+    if (form.invalid) {
+      this.validateAllFormFields(form.control);
+      this.snotifyService.warning('Please Fill All Fields');
+      return;
+    }
+    this.supplier.productVariants.push(this.product_variant);
+    this.isAddorEditSKU = !this.isAddorEditSKU;
+    console.log(this.supplier.productVariants);
   }
 
   // end
@@ -215,18 +227,18 @@ export class ProductComponent implements OnInit, OnDestroy {
       this.productService.onProductChanged.next(data);
 
       // Show the success message
-      this.snotifyService.success("Product added", "Success !");
+      this.snotifyService.success('Product added', 'Success !');
       // Change the location with new one
       this.spinnerService.requestInProcess(false);
 
-      this.location.go("/products");
+      this.location.go('/products');
     });
   }
 
   addProduct(form) {
     if (form.invalid) {
       this.validateAllFormFields(form.control);
-      this.snotifyService.warning("Please Fill All Fields");
+      this.snotifyService.warning('Please Fill All Fields');
       return;
     }
     this.product.suppliers.push(this.supplier);
@@ -236,17 +248,17 @@ export class ProductComponent implements OnInit, OnDestroy {
 
     this.productService.addProduct(this.product).subscribe(
       (res: any) => {
-        this.snotifyService.success(res.res.message, "Success !");
+        this.snotifyService.success(res.res.message, 'Success !');
         this.spinnerService.requestInProcess(false);
         this.product = new Product();
         this.supplier = new Supplier();
-        this.router.navigate(["/products"]);
+        this.router.navigate(['/products']);
       },
       errors => {
         this.spinnerService.requestInProcess(false);
         let e = errors.error;
         e = JSON.stringify(e.error);
-        this.snotifyService.error(e, "Error !");
+        this.snotifyService.error(e, 'Error !');
       }
     );
   }
@@ -264,7 +276,7 @@ export class ProductComponent implements OnInit, OnDestroy {
         this.spinnerService.requestInProcess(false);
         let e = errors.error;
         e = JSON.stringify(e.error);
-        this.snotifyService.error(e, "Error !");
+        this.snotifyService.error(e, 'Error !');
         // this.notificationServiceBus.launchNotification(true, e);
       }
     );
@@ -282,7 +294,7 @@ export class ProductComponent implements OnInit, OnDestroy {
         this.spinnerService.requestInProcess(false);
         let e = errors.error;
         e = JSON.stringify(e.error);
-        this.snotifyService.error(e, "Error !");
+        this.snotifyService.error(e, 'Error !');
         // this.notificationServiceBus.launchNotification(true, e);
       }
     );
@@ -300,7 +312,7 @@ export class ProductComponent implements OnInit, OnDestroy {
         this.spinnerService.requestInProcess(false);
         let e = errors.error;
         e = JSON.stringify(e.error);
-        this.snotifyService.error(e, "Error !");
+        this.snotifyService.error(e, 'Error !');
         // this.notificationServiceBus.launchNotification(true, e);
       }
     );
@@ -318,7 +330,7 @@ export class ProductComponent implements OnInit, OnDestroy {
         this.spinnerService.requestInProcess(false);
         let e = errors.error;
         e = JSON.stringify(e.error);
-        this.snotifyService.error(e, "Error !");
+        this.snotifyService.error(e, 'Error !');
         // this.notificationServiceBus.launchNotification(true, e);
       }
     );
@@ -374,9 +386,9 @@ export class ProductComponent implements OnInit, OnDestroy {
 
   newContact() {
     this.dialogRef = this.dialog.open(FuseOptionFormDialogComponent, {
-      panelClass: "contact-form-dialog",
+      panelClass: 'contact-form-dialog',
       data: {
-        action: "new"
+        action: 'new'
       }
     });
 
@@ -392,12 +404,12 @@ export class ProductComponent implements OnInit, OnDestroy {
   }
   openRelatedTab(val: string) {
     switch (val) {
-      case "rulesTab":
+      case 'rulesTab':
         this.rulesTab = true;
         this.optionsTab = false;
         this.skusTab = false;
         break;
-      case "skusTab":
+      case 'skusTab':
         this.rulesTab = false;
         this.optionsTab = false;
         this.skusTab = true;
