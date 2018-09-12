5611792953c6a9e4de37f17b76954af7316a10b6
saimzishan@gmail.com <saimzishan@gmail.com>
2018-09-12 20:45:53 +0500

    WIP: category listing

--- src/app/main/content/categories/categories.component.ts
+++ src/app/main/content/categories/categories.component.ts
@@ -1,10 +1,8 @@
 import { SnotifyService } from "ng-snotify";
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { CategoriesService } from "./categories.service";
-import { DataSource } from "@angular/cdk/collections";
-import { Observable } from "rxjs/Observable";
 import { fuseAnimations } from "../../../core/animations";
-import { MatPaginator, MatSort } from "@angular/material";
+import { MatPaginator, MatSort, MatTableDataSource } from "@angular/material";
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
 import "rxjs/add/operator/startWith";
 import "rxjs/add/observable/merge";
@@ -15,7 +13,6 @@ import "rxjs/add/observable/fromEvent";
 import { FuseUtils } from "../../../core/fuseUtils";
 import { ITreeOptions, TreeNode, TreeComponent } from "angular-tree-component";
 import { SpinnerService } from "../../../spinner/spinner.service";
-
 @Component({
   selector: "app-categories",
   templateUrl: "./categories.component.html",
@@ -23,10 +20,6 @@ import { SpinnerService } from "../../../spinner/spinner.service";
   animations: fuseAnimations
 })
 export class CategoriesComponent implements OnInit {
-  dataSource: FilesDataSource | null;
-
-  @ViewChild(MatPaginator)
-  paginator: MatPaginator;
   @ViewChild("filter")
   filter: ElementRef;
   @ViewChild(MatSort)
@@ -34,9 +27,19 @@ export class CategoriesComponent implements OnInit {
   @ViewChild(TreeComponent)
   private tree: TreeComponent;
   category;
+  showChild = false;
+  dataSource: any;
   options: ITreeOptions = {
     getChildren: this.getChildren.bind(this)
   };
+  displayedColumns: string[] = ["name", "children", "notes"];
+  panelOpenState = false;
+  // displayedColumns = ["position", "name", "weight", "symbol"];
+  // dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
+
   newNodes;
   nodes: any[] = [];
 
@@ -49,64 +52,24 @@ export class CategoriesComponent implements OnInit {
       name: "child2"
     }
   ];
+  categories: any;
+  categoryChildren: any;
 
   constructor(
     private categoriesService: CategoriesService,
     private spinnerService: SpinnerService,
     private snotifyService: SnotifyService
-  ) {
-    // this.nodes = [
-    //   {
-    //     name: "root1",
-    //     children: [{ name: "child1" }]
-    //   },
-    //   {
-    //     name: "root2",
-    //     hasChildren: true
-    //   },
-    //   {
-    //     name: "root3"
-    //   }
-    // ];
-  }
+  ) {}
 
   ngOnInit() {
-    /*this.dataSource = new FilesDataSource(this.categoriesService, this.paginator, this.sort);
-    Observable.fromEvent(this.filter.nativeElement, 'keyup')
-      .debounceTime(150)
-      .distinctUntilChanged()
-      .subscribe(() => {
-        if ( !this.dataSource )
-        {
-          return;
-        }
-        this.dataSource.filter = this.filter.nativeElement.value;
-      });*/
     this.index();
-    this.dataSource = new FilesDataSource(
-      this.categoriesService,
-      this.paginator,
-      this.sort
-    );
-    Observable.fromEvent(this.filter.nativeElement, "keyup")
-      .debounceTime(150)
-      .distinctUntilChanged()
-      .subscribe(() => {
-        if (!this.dataSource) {
-          return;
-        }
-        this.dataSource.filter = this.filter.nativeElement.value;
-      });
   }
+
   createNode(obj) {
     let tempNode = [];
     obj.forEach(row => {
       let tempObj = {};
       if (row.children.length > 0) {
-        // let cNode = [];
-        // row.children.forEach(element => {
-        //   cNode.push({ name: row.name });
-        // });
         tempObj = {
           name: row.name,
           hasChildren: true,
@@ -120,6 +83,9 @@ export class CategoriesComponent implements OnInit {
     });
     return tempNode;
   }
+  openChildren() {
+    this.showChild = !this.showChild;
+  }
   getChildren(node: any) {
     this.show(node.data.my_id);
     return new Promise((resolve, reject) => {
@@ -127,13 +93,19 @@ export class CategoriesComponent implements OnInit {
     });
   }
 
+  setDataSource(obj) {
+    this.dataSource = new MatTableDataSource<any>(obj);
+    this.dataSource.paginator = this.paginator;
+  }
+
   index() {
     this.spinnerService.requestInProcess(true);
     this.categoriesService.index().subscribe(
       (res: any) => {
         if (!res.status) {
-          this.category = res.res.data;
-          this.nodes = this.createNode(this.category);
+          this.categories = res.res.data;
+          this.nodes = this.createNode(this.categories);
+          this.setDataSource(this.categories);
         }
         this.spinnerService.requestInProcess(false);
       },
@@ -153,10 +125,8 @@ export class CategoriesComponent implements OnInit {
       (res: any) => {
         this.spinnerService.requestInProcess(false);
         if (!res.status) {
-          this.category = res.res.data;
-
-          this.asyncChildren = this.createNode(this.category);
-          // this.tree.treeModel.update();
+          this.categoryChildren = res.res.data;
+          this.asyncChildren = this.createNode(this.categoryChildren);
 
           this.newNodes = this.asyncChildren.map(c => Object.assign({}, c));
         }
@@ -170,106 +140,4 @@ export class CategoriesComponent implements OnInit {
       }
     );
   }
-}
-
-export class FilesDataSource extends DataSource<any> {
-  _filterChange = new BehaviorSubject("");
-  _filteredDataChange = new BehaviorSubject("");
-
-  get filteredData(): any {
-    return this._filteredDataChange.value;
-  }
-
-  set filteredData(value: any) {
-    this._filteredDataChange.next(value);
-  }
-
-  get filter(): string {
-    return this._filterChange.value;
-  }
-
-  set filter(filter: string) {
-    this._filterChange.next(filter);
-  }
-
-  constructor(
-    private categoriesService: CategoriesService,
-    private _paginator: MatPaginator,
-    private _sort: MatSort
-  ) {
-    super();
-    this.filteredData = this.categoriesService.suppliers;
-  }
-
-  /** Connect function called by the table to retrieve one stream containing the data to render. */
-  connect(): Observable<any[]> {
-    const displayDataChanges = [
-      this.categoriesService.onProductsChanged,
-      this._paginator.page,
-      this._filterChange,
-      this._sort.sortChange
-    ];
-
-    return Observable.merge(...displayDataChanges).map(() => {
-      let data = this.categoriesService.suppliers.slice();
-
-      data = this.filterData(data);
-
-      this.filteredData = [...data];
-
-      data = this.sortData(data);
-
-      // Grab the page's slice of data.
-      const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
-      return data.splice(startIndex, this._paginator.pageSize);
-    });
-  }
-
-  filterData(data) {
-    if (!this.filter) {
-      return data;
-    }
-    return FuseUtils.filterArrayByString(data, this.filter);
-  }
-
-  sortData(data): any[] {
-    if (!this._sort.active || this._sort.direction === "") {
-      return data;
-    }
-
-    return data.sort((a, b) => {
-      let propertyA: number | string = "";
-      let propertyB: number | string = "";
-
-      switch (this._sort.active) {
-        case "id":
-          [propertyA, propertyB] = [a.id, b.id];
-          break;
-        case "name":
-          [propertyA, propertyB] = [a.name, b.name];
-          break;
-        case "categories":
-          [propertyA, propertyB] = [a.categories[0], b.categories[0]];
-          break;
-        case "price":
-          [propertyA, propertyB] = [a.priceTaxIncl, b.priceTaxIncl];
-          break;
-        case "quantity":
-          [propertyA, propertyB] = [a.quantity, b.quantity];
-          break;
-        case "active":
-          [propertyA, propertyB] = [a.active, b.active];
-          break;
-      }
-
-      const valueA = isNaN(+propertyA) ? propertyA : +propertyA;
-      const valueB = isNaN(+propertyB) ? propertyB : +propertyB;
-
-      return (
-        (valueA < valueB ? -1 : 1) * (this._sort.direction === "asc" ? 1 : -1)
-      );
-    });
-  }
-
-  disconnect() {}
 }
